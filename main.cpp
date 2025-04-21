0bbe56e5e09b457f46a203175f4c18c2ad5d359b
Max B. <maksym_borovyk@ukr.net>
2025-04-21 14:23:53 +0300

    Remove redundant writing to file

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <filesystem>
 
-constexpr double VIDEO_SAVE_RESIZE_COEF = 0.5;
 // constexpr const char* INPUT_VIDEO_PATH = "./data/advio-01/iphone/frames.mov";
 constexpr const char* INPUT_VIDEO_PATH = "./data/road.mp4";
 constexpr int GUESSED_FOCAL_LENGTH = 700;
@@ -14,11 +13,6 @@ static cv::Mat pose = cv::Mat::eye(4, 4, CV_64F);
 static int inlier_count = 0;
 static int matches_count = 0;
 
-struct SavedVideoParams {
-    int frame_width;
-    int frame_height;
-    double fps;
-};
 
 void calculate_keypoints(cv::Ptr<cv::ORB> orb, const cv::Mat& frame, std::vector<cv::KeyPoint>& keypoints, cv::Mat& descriptors){
     cv::Mat gray;
@@ -83,42 +77,6 @@ void calculate_pose(
 
 }
 
-struct SavedVideoParams get_params(const cv::VideoCapture& cap){ 
-    struct SavedVideoParams params;
-    params.frame_width = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_WIDTH) * VIDEO_SAVE_RESIZE_COEF);
-    params.frame_height = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_HEIGHT) * VIDEO_SAVE_RESIZE_COEF);
-    params.fps = cap.get(cv::CAP_PROP_FPS);
-    return params;
-}
-
-bool initialize_writers(const struct SavedVideoParams& video_params, cv::VideoWriter& writer){
-
-    // writer for original video with marks
-    writer.open(
-        "./output/output.mp4", 
-        cv::VideoWriter::fourcc('m', 'p', '4', 'v'), // MP4 codec
-        video_params.fps, 
-        cv::Size(video_params.frame_width, video_params.frame_height)
-    );
-
-    if (!writer.isOpened()) {
-        std::cerr << "Error: Cannot open the video writer.\n" << std::endl;
-        return false;
-    }
-
-    // if success return true
-    return true;
-}
-
-void write_to_file(const cv::Mat& frame, const struct SavedVideoParams& video_params, cv::VideoWriter& writer, bool is_grey){
-    cv::Mat frame_tmp;
-    cv::resize(frame, frame_tmp, cv::Size(video_params.frame_width, video_params.frame_height));
-    if (is_grey){
-        cv::cvtColor(frame_tmp, frame_tmp, cv::COLOR_GRAY2BGR);
-    }
-    writer.write(frame_tmp);
-}
-
 void visualize_trajectory(cv::Mat& traj){
 
     // Extract camera translation (world position)
@@ -159,15 +117,6 @@ int main() {
         std::cerr << "Error: Cannot open the video file." << std::endl;
         return -1;
     }
-
-    // for saving video
-    struct SavedVideoParams video_params = get_params(cap);
-    cv::VideoWriter writer;
-    bool writer_init_is_successful = initialize_writers(video_params, writer);
-    if (!writer_init_is_successful){
-        std::cerr << "Error: Writers init wasn't successful." << std::endl;
-        return -1;
-    }
     
     cv::Mat traj = cv::Mat::zeros(600, 600, CV_8UC3);
 
@@ -194,6 +143,7 @@ int main() {
     std::vector<cv::KeyPoint> keypoints1;
     cv::Mat descriptors1;
     calculate_keypoints(orb, frame1, keypoints1, descriptors1);
+
     while (true) {
         // Read next frame
         cap >> frame2;
@@ -206,14 +156,14 @@ int main() {
         std::vector<cv::KeyPoint> keypoints2;
         cv::Mat descriptors2;
         calculate_keypoints(orb, frame2, keypoints2, descriptors2);
+
         calculate_pose(keypoints1, keypoints2, descriptors1, descriptors2, camera_intrinsics, keypoints_to_display);
         display_frame(frame2, keypoints_to_display);
-        write_to_file(frame2, video_params, writer, false);
+        visualize_trajectory(traj);
 
         frame1 = frame2;
         keypoints1 = keypoints2;
         descriptors1 = descriptors2;
-        visualize_trajectory(traj);
 
         // Wait for 30ms and break if 'q' is pressed
         if (cv::waitKey(30) == 'q')
@@ -222,7 +172,6 @@ int main() {
 
     // Release resources
     cap.release();
-    writer.release();
     cv::destroyAllWindows();
     return 0;
 }
